45ff73b778c6744818ad1dfc4aabed9982cbd537
Gian Marco Innocenti <94134814+gminnocenti@users.noreply.github.com>
2025-05-25 20:32:12 -0600

    complete azure deployment pipeline in azure ml workspace

--- azurepipelinedeployment/src/azurepipelinedeployment/pipelines/data_science/pipeline.py
+++ azurepipelinedeployment/src/azurepipelinedeployment/pipelines/data_science/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
-from .nodes import train_test_split_occupancy, sarimax
+from .nodes import train_test_split_occupancy, sarimax, train_test_split_demand, lightgbm_pca
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -14,13 +14,25 @@ def create_pipeline(**kwargs) -> Pipeline:
         func = train_test_split_occupancy,
         inputs = "daily_occupancy",
         outputs = ["X_train", "X_test", "y_train", "y_test"],
-        name = "train_test_split_occupancy_node",
+        name = "train_test_split_occupancy",
         ),
         node(
             func = sarimax,
             inputs = ["X_train", "y_train", "X_test", "y_test"],
             outputs = "results_occupancy",
             name = "sarimax_model"
+        ),
+        node(
+            func = train_test_split_demand,
+            inputs = "daily_demand",
+            outputs = ["X_train_demand", "X_test_demand", "y_train_demand", "y_test_demand", "horizon"],
+            name = "train_test_split_demand"
+        ),
+        node(
+            func = lightgbm_pca,
+            inputs = ["X_train_demand", "y_train_demand", "X_test_demand", "y_test_demand", "results_occupancy", "horizon","dishes_mapping"],
+            outputs = "results_demand",
+            name = "lightgbm_model"
         )
 
 
